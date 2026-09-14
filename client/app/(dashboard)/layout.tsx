fb0d550ba1f176e9df3e61e14474ad10f58610d3
Tashdik-29 <trk.ice153@gmail.com>
2026-09-14 17:54:08 +0600

    font updated

--- client/app/(dashboard)/layout.tsx
+++ client/app/(dashboard)/layout.tsx
@@ -4,55 +4,6 @@ import { DashboardSidebar } from '@/components/dashboard/dashboard-sidebar';
 import { AuthProvider, useAuth } from '@/features/auth/auth-provider';
 import { DashboardShell } from '@/components/dashboard/dashboard-shell';
 
-// function DashboardContent({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     const { isLoading, isAuthenticated } = useAuth();
-
-//     if (isLoading) {
-//         return (
-//             <div className="flex min-h-screen items-center justify-center">
-//                 <div className="text-sm text-muted-foreground">
-//                     Checking authentication...
-//                 </div>
-//             </div>
-//         );
-//     }
-
-//     if (!isAuthenticated) {
-//         return null;
-//     }
-
-//     return (
-//         <div className="min-h-screen bg-muted/30">
-//             <DashboardSidebar />
-
-//             <div className="pl-72">
-//                 <main className="min-h-screen p-6">
-//                     {children}
-//                 </main>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default function DashboardLayout({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     return (
-//         <AuthProvider>
-//             <DashboardContent>
-//                 {children}
-//             </DashboardContent>
-//         </AuthProvider>
-//     );
-// }
-
-
 
 
 
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className='font-Poppins'>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
